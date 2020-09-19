8a8d9aa5418fde28eb245e81066e783aac91cb75
Sarra Benloulou <sara.benloulou@gmail.com>
2020-09-19 11:53:15 +0200

    requetes

---
+++ GenieLog-M1-TP-src/GildedRose/commandes.md
@@ -0,0 +1,9 @@
+
+
+
+# ce mettre à l'interieur du dossier **GildedRose**, et executer la commande "./gradlew"
+
+# pour executer les testes on se lance la commande "./gradlew test"
+
+
+
---
+++ GenieLog-M1-TP-src/GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].name, is("foo"));
+    }
+
+    @Test
+    void foo2() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].name, is("foo"));
+    }
+}
