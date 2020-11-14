53bfdb3d1254cb91ec27c32a178ddf8ae56eeb97
Sarra Benloulou <sara.benloulou@gmail.com>
2020-11-14 21:06:05 +0100

    some cleaning in ItemTest.java

--- GenieLog-M1-TP-src/GildedRose/src/test/java/com/gildedrose/ItemTest.java
+++ GenieLog-M1-TP-src/GildedRose/src/test/java/com/gildedrose/ItemTest.java
@@ -6,30 +6,21 @@ import static org.hamcrest.Matchers.*;
 
 class ItemTest{
 
-
-/*#################################### Other Items testes #################################*/
-
-    // Tester le nom sellIN , quality
     @Test
     void testProperties(){
-        // name, sellIn, quality
         Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20) };
         GildedRose app = new GildedRose(items);
+
         assertThat(app.items[0].name, is( "+5 Dexterity Vest" ));
         assertThat(app.items[0].sellIn, is( 10));
         assertThat(app.items[0].quality, is(20));
     }
 
-     // test tostring
-     @Test
-     void testToString() {
-         
-         // name, sellIn, quality
-         
-         Item[] items = new Item[] { new Item("+5 Dexterity Vest", 5, 8) };
-         GildedRose app = new GildedRose(items);
- 
-         assertThat(app.items[0].toString(), is( "+5 Dexterity Vest, 5, 8" ));
-     }
+    @Test
+    void testToString() {        
+        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 5, 8) };
+        GildedRose app = new GildedRose(items);
 
+        assertThat(app.items[0].toString(), is( "+5 Dexterity Vest, 5, 8" ));
+    }
 }
