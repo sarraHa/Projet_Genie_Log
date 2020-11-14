a9c48fa9583858efdf2fb5fdad17d550a22ddbc4
Sarra Benloulou <sara.benloulou@gmail.com>
2020-11-14 17:22:21 +0100

    corrected the style errors of the file GildedRose.java

--- GenieLog-M1-TP-src/GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GenieLog-M1-TP-src/GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,42 +1,37 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
 
-    public static final String agedBrie = "Aged Brie";
-    public static final String backstage = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String sulfuras = "Sulfuras, Hand of Ragnaros";
-    public static final String conjured = "Conjured Mana Cake";
+    public static final String AGEDBRIE = "Aged Brie";
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String CONJURED = "Conjured Mana Cake";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    public GildedRose(Item[] items) {
+    public Item[] items;
+
+    GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-
         for (Item item : items) {
             GildedRoseItems gildedRoseItem = getItemByName(item);
             gildedRoseItem.update(item);
         }
     }
 
-    private GildedRoseItems getItemByName(Item item){
-        switch(item.name) {
-            case sulfuras:
-                Sulfuras sulfuras = new Sulfuras();
-                return sulfuras;
-            case agedBrie:
-                AgedBrie agedBrie = new AgedBrie();
-                return agedBrie;
-            case backstage:
-                Backstage backstage = new Backstage();
-                return backstage;
-            case conjured:
-                Conjured conjured = new Conjured();
-                return conjured;
+    private GildedRoseItems getItemByName(Item item) {
+        switch (item.name) {
+            case SULFURAS:
+                return new Sulfuras();
+            case AGEDBRIE:
+                return new AgedBrie();
+            case BACKSTAGE:
+                return new Backstage();
+            case CONJURED:
+                return new Conjured();
             default:
-                OtherItems otherItems = new OtherItems();
-                return otherItems;
+                return new OtherItems();
         }
     }
 }
