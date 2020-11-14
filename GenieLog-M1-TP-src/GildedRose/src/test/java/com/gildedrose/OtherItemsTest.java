d45e52bf8de4bb987d4ddc128070f8075c896519
Sarra Benloulou <sara.benloulou@gmail.com>
2020-11-14 21:15:24 +0100

    some cleaning in OtherItemsTest.java

--- GenieLog-M1-TP-src/GildedRose/src/test/java/com/gildedrose/OtherItemsTest.java
+++ GenieLog-M1-TP-src/GildedRose/src/test/java/com/gildedrose/OtherItemsTest.java
@@ -6,14 +6,8 @@ import static org.hamcrest.Matchers.*;
 
 class OtherItemsTest{
 
-
-
-    //Tester le SellIn d'un produit ordinaire ( sellIn > 0)
     @Test
-    void sellInProduitOrdinaire() {
-        
-        // name, sellIn, quality
-        
+    void sellInOrdinaryProduct() {            
         Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = app.items[0].quality - 1;
@@ -23,13 +17,9 @@ class OtherItemsTest{
         assertThat(app.items[0].quality, is( expectedQuality ));
         assertThat(app.items[0].sellIn, is( expectedSellIn ));
     }
-
-    //Tester la quality d'un produit ordinaire ( sellIn < 0) et qualité < 0
         
     @Test
     void ordinaryItemWithNegativeSellInAndNegativeQuality() {        
-        // name, sellIn, quality
-
         Item[] items = new Item[] { new Item("Elixir of the Mongoose", -1, -5) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = app.items[0].quality;
@@ -38,11 +28,8 @@ class OtherItemsTest{
         assertThat(app.items[0].quality, is( expectedQuality ));
     }
 
-    //La qualité (`quality`) d'un produit ne peut jamais être négative.
     @Test
-    void testNegativeQuality() {
-                
-        // name, sellIn, quality
+    void testNegativeQuality() {                
         Item[] items = new Item[] { new Item("Elixir of the Mongoose", 5, 0) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = 0;
@@ -51,11 +38,8 @@ class OtherItemsTest{
         assertThat(app.items[0].quality, is( expectedQuality )) ;
     }
 
-    //- Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.
     @Test
-    void qualitywhensellinisnegativ() {
-                
-        // name, sellIn, quality
+    void qualitywhensellinisnegativ() {                
         Item[] items = new Item[] { new Item("Elixir of the Mongoose", -2, 20) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = 20-2;
@@ -68,8 +52,7 @@ class OtherItemsTest{
 
     @Test
     void qualitywhenSellIn0() {
-                
-        // name, sellIn, quality
+
         Item[] items = new Item[] { new Item("Elixir of the Mongoose", 0, 2) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = 2-2;
@@ -79,7 +62,4 @@ class OtherItemsTest{
         assertThat(app.items[0].quality, is( expectedQuality )) ;
         assertThat(app.items[0].sellIn, is( expectedSellIn )) ;
     }
-
-
-
 }
