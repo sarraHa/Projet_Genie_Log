b9bc39604ad2ec59216ac32b5dbbed67ba0c4f8b
Sarra Benloulou <sara.benloulou@gmail.com>
2020-11-14 21:00:43 +0100

    some cleaning in BackstageTest.java

--- GenieLog-M1-TP-src/GildedRose/src/test/java/com/gildedrose/BackstageTest.java
+++ GenieLog-M1-TP-src/GildedRose/src/test/java/com/gildedrose/BackstageTest.java
@@ -6,19 +6,13 @@ import static org.hamcrest.Matchers.*;
 
 class BackstageTest{
 
-
     @Test
 	void mustNotBeEqualToNull() {
         assertThat(new Backstage(), is(notNullValue()));
     }
 
- //################ Backstage testes ##########################"/
-    
-    //- La qualité d'un produit n'est jamais de plus de 50.
     @Test
-    void qualityNotsuperiorto50() {
-                
-        // name, sellIn, quality
+    void qualityNotsuperiorto50() {                
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50) };
         GildedRose app = new GildedRose(items);
         int expectedQuality= 50;
@@ -26,14 +20,10 @@ class BackstageTest{
 
         assertThat(app.items[0].quality, is( expectedQuality)) ;
         assertThat(app.items[0].sellIn, is( 4)) ;
-
     }
 
-    //"Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
     @Test
-    void qualityBackstagePasses() {
-                
-        // name, sellIn, quality
+    void increaseQuality() {                
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
         GildedRose app = new GildedRose(items);
         int expectedQuality= 21;
@@ -41,31 +31,10 @@ class BackstageTest{
 
         assertThat(app.items[0].quality, is( expectedQuality)) ;
         assertThat(app.items[0].sellIn, is( 14)) ;
-
     }
 
-    //"Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
     @Test
-    void qualityBackstagePasses_1() {
-                
-        // name, sellIn, quality
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
-        GildedRose app = new GildedRose(items);
-        int expectedQuality = 21;
-        app.updateQuality();
-
-        assertThat(app.items[0].quality, is( expectedQuality)) ;
-        assertThat(app.items[0].sellIn, is( 14)) ;
-
-    }
-
-
-
-    //"Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
-    @Test
-    void qualityBackstagePasses_2() {
-                
-        // name, sellIn, quality
+    void qualityWhen10DaysLeft() {                
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = 22;
@@ -73,14 +42,10 @@ class BackstageTest{
 
         assertThat(app.items[0].quality, is( expectedQuality)) ;
         assertThat(app.items[0].sellIn, is( 8)) ;
-
     }
 
-    //"Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
     @Test
-    void qualityBackstagePasses_3() {
-                
-        // name, sellIn, quality
+    void qualityWhen5DaysLeft() {                
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 4, 10) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = 10 + 3;
@@ -88,16 +53,10 @@ class BackstageTest{
 
         assertThat(app.items[0].quality, is( expectedQuality)) ;
         assertThat(app.items[0].sellIn, is( 3)) ;
-
     }
 
-
-    //"Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
-    //la quality ne peut pas etre plus de 50
     @Test
-    void qualityBackstageAfterConcert() {
-                
-        // name, sellIn, quality
+    void qualityAfterConcert() {                
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = 0;
@@ -105,15 +64,10 @@ class BackstageTest{
 
         assertThat(app.items[0].quality, is( expectedQuality)) ;
         assertThat(app.items[0].sellIn, is( -1)) ;
-
     }
 
-    //"Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
-    //la quality ne peut pas etre plus de 50
     @Test
-    void qualityBackstagePassesSuperiorto50() {
-                
-        // name, sellIn, quality
+    void qualityNotSuperiorto50() {                
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 4, 49) };
         GildedRose app = new GildedRose(items);
         int expectedQuality = 50;
@@ -121,10 +75,5 @@ class BackstageTest{
 
         assertThat(app.items[0].quality, is( expectedQuality)) ;
         assertThat(app.items[0].sellIn, is( 3)) ;
-
     }
-    
-    //################ end Backstage testes ##########################"/
-
-
 }
